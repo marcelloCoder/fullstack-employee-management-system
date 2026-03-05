09498079dfe3d03d852cf6e89415c7fb42d9ac9f
marcelloCoder <deoliveiral767@gmail.com>
2026-03-05 16:02:14 -0300

    adding routes

--- ems-frontend/src/App.jsx
+++ ems-frontend/src/App.jsx
@@ -3,15 +3,22 @@ import HelloWorld from './HelloWorld'
 import HeaderComponent from './components/HeaderComponent'
 import FooterComponent from './components/FooterComponent'
 import ListEmployeesComponent from './components/ListEmployeesComponent'
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
 
 function App() {
  
-
   return (
     <>
+    <Router>
       <HeaderComponent />
-      <ListEmployeesComponent />
+      <Routes>
+       { /*// localhost:3000 */}
+        <Route path='/' element={<ListEmployeesComponent />} />
+        {/*/ localhost:3000/employees */}
+        <Route path='/employees' element={<ListEmployeesComponent />} />
+      </Routes>
       <FooterComponent />
+    </Router>
     </>
   )
 }
