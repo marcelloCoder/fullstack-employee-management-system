6675d063a10169e63e9d36572ecd051e6a548de7
marcelloCoder <deoliveiral767@gmail.com>
2026-03-16 14:56:26 -0300

    add exception handlers

--- ems-frontend/src/components/EmployeeComponent.jsx
+++ ems-frontend/src/components/EmployeeComponent.jsx
@@ -9,23 +9,60 @@ function EmployeeComponent() {
   const [email, setEmail] = useState('')
   const navigate = useNavigate()
 
+  const [errors, setErrors] = useState({
+    firstName: '',
+    lastName: '',
+    email: ''
+  })
+
   function saveEmployee(event){
     event.preventDefault();
 
-    const employee = { firstName, lastName, email };
+    if (validateForm()) {
+      const employee = { firstName, lastName, email };
 
-    console.log("Saving employee:", employee);
-
-    createEmployee(employee)
-      .then((response) => {
-        console.log('Employee created:', response.data)
-        navigate('/employees')
-      })
-      .catch((error) => {
-        console.error('Error creating employee:', error)
-      })
+      console.log("Saving employee:", employee);
+  
+      createEmployee(employee)
+        .then((response) => {
+          console.log('Employee created:', response.data)
+          navigate('/employees')
+        })
+        .catch((error) => {
+          console.error('Error creating employee:', error)
+        })
+    }
   }
   
+  function validateForm() {
+    let valid = true;
+    const errorsCopy = { ...errors };
+
+    if (!firstName.trim()) {
+      errorsCopy.firstName = 'First name is required';
+      valid = false;
+    } else {
+      errorsCopy.firstName = '';
+    }
+
+    if (!lastName.trim()) {
+      errorsCopy.lastName = 'Last name is required';
+      valid = false;
+    } else {
+      errorsCopy.lastName = '';
+    }
+
+    if (!email.trim()) {
+      errorsCopy.email = 'Email is required';
+      valid = false;
+    } else {
+      errorsCopy.email = '';
+    }
+
+    setErrors(errorsCopy);
+    return valid;
+    
+  }
   
   return (
     <div className='container'>
@@ -39,28 +76,37 @@ function EmployeeComponent() {
                 <label className='form-label'>First Name:</label>
                 <input type='text'
                 placeholder='Enter first name'
-                 className='form-control'
+                 className={`form-control${errors.firstName ? ' is-invalid' : ''}`}
                  value={firstName}
                  onChange={(event) => setFirstName(event.target.value)}
                  />
+                 {errors.firstName && <div className="invalid-feedback">{errors.firstName}</div>}
               </div>
               <div className='form-group mb-2'>
                 <label className='form-label'>Last Name:</label>
-                <input type='text'
-                placeholder='Enter last name'
-                 className='form-control'
-                 value={lastName}
-                 onChange={(event) => setLastName(event.target.value)} />
+                <input
+                  type='text'
+                  placeholder='Enter last name'
+                  className={`form-control${errors.lastName ? ' is-invalid' : ''}`}
+                  value={lastName}
+                  onChange={(event) => setLastName(event.target.value)}
+                />
+                {errors.lastName && (
+                  <div className="invalid-feedback">{errors.lastName}</div>
+                )}
               </div>
               <div className='form-group mb-2'>
                 <label className='form-label'>Email:</label>
                 <input
                   type='email'
                   placeholder='Enter email'
-                  className='form-control'
+                  className={`form-control${errors.email ? ' is-invalid' : ''}`}
                   value={email}
                   onChange={(event) => setEmail(event.target.value)}
                 />
+                {errors.email && (
+                  <div className="invalid-feedback">{errors.email}</div>
+                )}
               </div>
 
               <button type='submit' className='btn btn-primary' onClick={saveEmployee}>
